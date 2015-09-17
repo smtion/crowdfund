8b343787aef582bce5af59e8d3400ece73c59695
Dev <dev@Choiui-MacBook-Air.local>
2015-09-17 14:08:08 +0900

    Implement API controller

---
+++ backend/application/helpers/api_helper.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function return_json($res)
+{
+	$CI =& get_instance();
+	$res = json_encode($res);
+	$CI->output->set_content_type('application/json');
+	echo $res;
+}
